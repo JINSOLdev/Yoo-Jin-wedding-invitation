ce0cc19a7f247dc673f692857f6e93dede1b8018
Jinsol Kim <jinsol032360@gmail.com>
2024-02-13 13:59:29 +0900

    fix. 모듈명 수정

--- yoo-jin/app/page.js
+++ yoo-jin/app/page.js
@@ -1,42 +1,42 @@
-import { Intro } from "./components/Intro";
-import { DateOne } from "./components/dateOne";
-import { Lyrics } from "./components/Lyrics";
-import { GroomBride } from "./components/GroomBride";
-import { DateLocation } from "./components/DateLocation";
-import { ThanksTo } from "./components/ThanksTo";
-import { Contact } from "./components/Contact";
-import { Account } from "./components/Account";
-import { DateTwo } from "./components/DateTwo";
-import { Location } from "./components/Location";
-import { Transport } from "./components/Transport";
-import { Gallery } from "./components/Gallery";
+import { Intro } from './components/Intro';
+import { DateOne } from './components/DateOne';
+import { Lyrics } from './components/Lyrics';
+import { GroomBride } from './components/GroomBride';
+import { DateLocation } from './components/DateLocation';
+import { ThanksTo } from './components/ThanksTo';
+import { Contact } from './components/Contact';
+import { Account } from './components/Account';
+import { DateTwo } from './components/DateTwo';
+import { Location } from './components/Location';
+import { Transport } from './components/Transport';
+import { Gallery } from './components/Gallery';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <div className="text">THE WEDDING DAY</div>
-      <img src={`/picture01.jpg`} className="picture"></img>
-      <Intro />
-      <DateOne />
-      <hr></hr>
-      <Lyrics />
-      <GroomBride />
-      <DateLocation />
-      <hr></hr>
-      <ThanksTo />
-      <hr></hr>
-      <Contact />
-      <hr></hr>
-      <Account />
-      <hr></hr>
-      <DateTwo />
-      <hr></hr>
-      <Location />
-      <hr></hr>
-      <Transport />
-      <hr></hr>
-      <Gallery />
-      <p className="rights">© 2024-04-06 Yoohwan, Jinsol</p>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="text">THE WEDDING DAY</div>
+            <img src={`/picture01.jpg`} className="picture"></img>
+            <Intro />
+            <DateOne />
+            <hr></hr>
+            <Lyrics />
+            <GroomBride />
+            <DateLocation />
+            <hr></hr>
+            <ThanksTo />
+            <hr></hr>
+            <Contact />
+            <hr></hr>
+            <Account />
+            <hr></hr>
+            <DateTwo />
+            <hr></hr>
+            <Location />
+            <hr></hr>
+            <Transport />
+            <hr></hr>
+            <Gallery />
+            <p className="rights">© 2024-04-06 Yoohwan, Jinsol</p>
+        </div>
+    );
 }
