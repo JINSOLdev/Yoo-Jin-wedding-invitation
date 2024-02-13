82fdab2cf48d40b482bc6e6ee402c806e2fcea63
Jinsol Kim <jinsol032360@gmail.com>
2024-02-13 17:48:45 +0900

    fix. 청첩장 내용 1차 수정

--- yoo-jin/app/components/Contact.js
+++ yoo-jin/app/components/Contact.js
@@ -1,9 +1,23 @@
 export function Contact() {
-  return (
-    <div className="contact">
-      연락처
-      <div className="phone">전화아이콘</div>
-      <div className="message">메시지아이콘</div>
-    </div>
-  );
+    return (
+        <div className="contact">
+            <span>연락처</span>
+            <div className="contactSections">
+                <div className="contactGroom">
+                    신랑측
+                    <div style={{ backgroundColor: 'aliceblue', marginTop: '60px' }}>
+                        <div>아빠</div>
+                        <div>엄마</div>
+                    </div>
+                </div>
+                <div className="contactBride">
+                    신부측
+                    <div style={{ backgroundColor: 'aliceblue', marginTop: '60px' }}>
+                        <div>아빠</div>
+                        <div>엄마</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- yoo-jin/app/components/DateTwo.js
+++ yoo-jin/app/components/DateTwo.js
@@ -1,10 +1,11 @@
 export function DateTwo() {
-  return (
-    <div className="dateTwo">
-      날짜<br></br>
-      달력<br></br>
-      <br></br>
-      <div id="calendar">4월</div>
-    </div>
-  );
+    return (
+        <div className="dateTwo">
+            날짜<br></br>
+            <br></br>
+            <div id="calendar">
+                4월<br></br>
+            </div>
+        </div>
+    );
 }
