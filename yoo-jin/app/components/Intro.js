6b6d4230ba1140156825f5abd1a53d44c3c7846b
Jinsol Kim <jinsol032360@gmail.com>
2024-02-14 14:26:23 +0900

    인트로 수정

--- yoo-jin/app/components/Intro.js
+++ yoo-jin/app/components/Intro.js
@@ -1,8 +1,10 @@
 export function Intro() {
-  return (
-    <div className="intro">
-      신유환, 김진솔 <br></br>
-      저희 결혼합니다<br></br>
-    </div>
-  );
+    return (
+        <div className="main-info">
+            <div className="main-info-title">
+              <p>The Marriage Of</p>
+              <p>신유환 & 김진솔</p>
+            </div>
+        </div>
+    );
 }
