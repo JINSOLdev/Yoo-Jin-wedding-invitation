564ef04e31628b7a65c829571f358357bc9374e5
Jinsol Kim <jinsol032360@gmail.com>
2024-02-15 17:29:56 +0900

    연락처 css 수정

--- yoo-jin/app/components/Contact.js
+++ yoo-jin/app/components/Contact.js
@@ -1,81 +1,98 @@
 export default function Contact() {
-  return (
-    <div className="contact">
-      <div className="grooms">
-        <p>신랑측</p>
-        <div className="grooms-contact">
-          <div>
-            <p>아버지 신희찬</p>
-            <div className="buttons parents"></div>
-            <div className="contact-1-call">
-              <a href="tell:010-8258-0870">
-                <div className="icon">
-                  <img src="/phone.svg"></img>
+    return (
+        <div className="contact">
+            <p className="num">연락처</p>
+            <div className="contact-box">
+                <div className="grooms">
+                    <p>신랑측</p>
+                    <div className="grooms-contact">
+                        <div>
+                            <p>아버지 신희찬</p>
+                            <div className="buttons parents">
+                                <div className="contact-1-call">
+                                    <a href="tell:010-8258-0870">
+                                        <div className="icon">
+                                            <img src="/phone.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div className="contact-1-msg">
+                                    <a href="sms:010-8258-0870">
+                                        <div className="icon">
+                                            <img src="/envelope.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p>어머니 정수희</p>
+                            <div className="buttons parents">
+                                <div className="contact-1-call">
+                                    <a href="tell:010-9590-2541">
+                                        <div className="icon">
+                                            <img src="/phone.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div className="contact-1-msg">
+                                    <a href="sms:010-9590-2541">
+                                        <div className="icon">
+                                            <img src="/envelope.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </a>
-            </div>
-            <div className="contact-1-msg">
-              <a href="sms:010-8258-0870">
-                <img src="/envelope.svg"></img>
-              </a>
-            </div>
-          </div>
-          <div>
-            <p>어머니 정수희</p>
-            <div className="buttons parents"></div>
-            <div className="contact-1-call">
-              <a href="tell:010-9590-2541">
-                <div className="icon">
-                  <img src="/phone.svg"></img>
+                <div className="brides">
+                    <p>신부측</p>
+                    <div className="grooms-contact">
+                        <div>
+                            <p>아버지 김명섭</p>
+                            <div className="buttons parents">
+                                <div className="contact-1-call">
+                                    <a href="tell:010-4797-2691">
+                                        <div className="icon">
+                                            <img src="/phone.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div className="contact-1-msg">
+                                    <a href="sms:010-4797-2691">
+                                        <div className="icon">
+                                            <img src="/envelope.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p>어머니 정순애</p>
+                            <div className="buttons parents">
+                                <div className="contact-1-call">
+                                    <a href="tell:010-6243-2693">
+                                        <div className="icon">
+                                            <img src="/phone.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div className="contact-1-msg">
+                                    <a href="sms:010-6243-2693">
+                                        <div className="icon">
+                                            <img src="/envelope.svg"></img>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </a>
             </div>
-            <div className="contact-1-msg">
-              <a href="sms:010-9590-2541">
-                <img src="/envelope.svg"></img>
-              </a>
+            <div>
+                <div className="divide-line"></div>
             </div>
-          </div>
         </div>
-      </div>
-      <div className="brides">
-        <p>신부측</p>
-        <div className="grooms-contact">
-          <div>
-            <p>아버지 김명섭</p>
-            <div className="buttons parents"></div>
-            <div className="contact-1-call">
-              <a href="tell:010-4797-2691">
-                <div className="icon">
-                  <img src="/phone.svg"></img>
-                </div>
-              </a>
-            </div>
-            <div className="contact-1-msg">
-              <a href="sms:010-4797-2691">
-                <img src="/envelope.svg"></img>
-              </a>
-            </div>
-          </div>
-          <div>
-            <p>어머니 정순애</p>
-            <div className="buttons parents"></div>
-            <div className="contact-1-call">
-              <a href="tell:010-6243-2693">
-                <div className="icon">
-                  <img src="/phone.svg"></img>
-                </div>
-              </a>
-            </div>
-            <div className="contact-1-msg">
-              <a href="sms:010-6243-2693">
-                <img src="/envelope.svg"></img>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="divide-lines"></div>
-    </div>
-  );
+    );
 }
