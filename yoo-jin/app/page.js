673e496d6c45bf67d1184f8ad1575390652252f9
Jinsol Kim <jinsol032360@gmail.com>
2024-02-19 15:35:28 +0900

    주석 삭제

--- yoo-jin/app/page.js
+++ yoo-jin/app/page.js
@@ -8,13 +8,7 @@ import Map from './components/Map';
 import Transport from './components/Transport';
 import Gallery from './components/Gallery';
 import Notice from './components/Notice';
-// 프론트 작업을 할때에 기본적인 부분은 시맨틱 요소로 구성하는 습관을 들이는 것이 좋음.
-// 그리고 reset.css를 사용하여 기본적인 스타일을 초기화하는 것이 좋음.
-// reset.css는 각자 쓰는 부분을 레퍼런스로 갖고 있는데 찾아보던가 아니면 직접 만들어서 사용하는 것이 좋음.(필요하면 요청하면 보내드릴 수 있음.)
 
-// 디렉토링을 할때에 src를 root로 두고 그 안에 components, pages, styles, utils 등을 두는 것이 좋다고 생각함.(구분이 쉬움)
-
-// 닫는 요소에 대해서 알아보고 <img/>와 같이 children 노드가 필요없을 경우 바로 닫는 요소를 사용하는 것이 가독성에 좋음.
 export default function Home() {
     return (
         <div className="container">
