209b4bfc6e706443326aef2a90abe429c5ffecd6
Jinsol Kim <jinsol032360@gmail.com>
2024-02-22 13:49:09 +0900

    fix : viewport

--- yoo-jin/app/layout.js
+++ yoo-jin/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="ko">
             <Head>
-                <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <body className={inter.className}>{children}</body>
         </html>
