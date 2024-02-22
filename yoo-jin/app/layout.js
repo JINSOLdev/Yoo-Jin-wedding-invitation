4dbf7d8c36e1a25f5969e7609ca6a38a484b0972
Jinsol Kim <jinsol032360@gmail.com>
2024-02-22 14:46:32 +0900

    add : viewport

--- yoo-jin/app/layout.js
+++ yoo-jin/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="ko">
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+            </Head>
             <body className={inter.className}>{children}</body>
         </html>
     );
