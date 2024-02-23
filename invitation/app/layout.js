7b4ed70c2dd9218a53da5166de080011d2841769
Jinsol Kim <jinsol032360@gmail.com>
2024-02-23 11:54:37 +0900

    fix : userScalable -> true

--- invitation/app/layout.js
+++ invitation/app/layout.js
@@ -15,7 +15,7 @@ export const viewport = {
     width: 'device-width',
     initialScale: 1,
     maximumScale: 1,
-    userScalable: false,
+    userScalable: true,
 };
 
 export default function RootLayout({ children }) {
