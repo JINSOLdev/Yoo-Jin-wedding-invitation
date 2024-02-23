2e7da5e73372eb111e138dfc606afcd753884ae4
Jinsol Kim <jinsol032360@gmail.com>
2024-02-23 11:49:25 +0900

    add : export viewport

--- invitation/app/layout.js
+++ invitation/app/layout.js
@@ -1,20 +1,26 @@
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-    title: "YooWhan & Jinsol's Wedding",
-    description: 'Generated  by create next app',
+    openGraph: {
+        title: "YooWhan & Jinsol's Wedding",
+        description: 'Generated  by create next app',
+        url: 'mobile-invitation-ivory.vercel.app/',
+    },
+};
+
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="ko">
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-            </Head>
             <body className={inter.className}>{children}</body>
         </html>
     );
